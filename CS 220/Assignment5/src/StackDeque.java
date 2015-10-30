746b6f1cf8eb64a7bd790fa1dd4fa84418cd94e8
root <root@localhost.localdomain>
2015-10-29 22:53:03 -0500

    CS Fun May be wrong

--- CS 220/Assignment5/src/StackDeque.java
+++ CS 220/Assignment5/src/StackDeque.java
@@ -18,67 +18,66 @@ class QueuBase{
 	//All private because it only needs to be accessed within this class
 	private Node firstNode;
 	private Node lastNode;
-  private T data;
+  	private T data;
 
 	/**It's protected because you only want to have your child classes to
 	to be able to see and have the access to add objects to the queues
-   */
-  protected void addToFront(T newEntry);
-  /**It's protected because only the classes that inherit this class
-   need access to the this method, method wouldn't be used for stack
-   */
-  protected void addToBack(T newEntry);
-  /*Protected because it removes the front of the queue,
-    You need your child classes to be able to remove from front of queue,
-    method wouldn't be used for stack
-   */
-  protected T removeFront();
-  /*Protected because it removes the back of the queue,
-    You need your child classes to be able to be able to remove from back of queue
-    or last part of stack
-   */
-  protected T removeBack();
-  /**Should be public because all it's 
-    doing is getting the front of the queue
-   */
-  public T getFront();
-  /**It's public because the public needs to have the ability
-    to get the back of the queue
-   */
-  public T getBack();
-  /**It's public because all it's doing is checking
-    if the queue is Empty or not 
-   */
-  public boolean isEmpty();
-  /**It's protected because only the child classes need to have the ability to
-    make the queue, deque, or stack empty
-   */
-  protected void clear();
-  /**private because it only needs to 
-  be accessed within this class
-  */
-  private class Node{
+   	*/
+  	protected void addToFront(T newEntry);
+  	/**It's protected because only the classes that inherit this class
+   	need access to the this method, method wouldn't be used for stack
+   	*/
+ 	protected void addToBack(T newEntry);
+  	/*Protected because it removes the front of the queue,
+    	You need your child classes to be able to remove from front of queue,
+    	method wouldn't be used for stack
+   	*/
+  	protected T removeFront();
+  	/*Protected because it removes the back of the queue,
+    	You need your child classes to be able to be able to remove from back of queue
+    	or last part of stack
+   	*/
+  	protected T removeBack();
+  	/**Should be public because all it's 
+    	doing is getting the front of the queue
+   	*/
+  	public T getFront();
+  	/**It's public because the public needs to have the ability
+    	to get the back of the queue
+   	*/
+  	public T getBack();
+  	/**It's public because all it's doing is checking
+    	if the queue is Empty or not 
+   	*/
+  	public boolean isEmpty();
+  	/**It's protected because only the child classes need to have the ability to
+    	make the queue, deque, or stack empty
+   	*/
+  	protected void clear();
+  	/**private because it only needs to 
+  	be accessed within this class
+  	*/
+  	private class Node{
 
-  }
-  public class Queue<T> extends QueueBase<T> {
+  	}
+  	public class Queue<T> extends QueueBase<T> {
 
-    public void offer(T newEntry) {
-      Node newNode = new Node(lastNode, newEntry, null);
-      lastNode = newNode;
-    }
-  }
+    		public void offer(T newEntry) {
+      			Node newNode = new Node(lastNode, newEntry, null);
+      			lastNode = newNode;
+    		}
+  	}	
 
-  public class Stack<T> extends QueueBase<T> {
+  	public class Stack<T> extends QueueBase<T> {
+		public void push(T newEntry) {
+      			Node newNode = new Node(null, newEntry, firstNode);
+      			firstNode = newNode;
+    		}
+  	}
 
-    public void push(T newEntry) {
-      Node newNode = new Node(null, newEntry, firstNode);
-      firstNode = newNode;
-    }
-  }
+  	public class Deque<T> extends QueueBase<T> {
 
-  public class Deque<T> extends QueueBase<T> {
-
-  }
+  	}	
 }
 
 
