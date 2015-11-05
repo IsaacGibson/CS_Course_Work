bfae13cb49ae78cb6f03ad37f2d251aaa88d52e6
phantomfreeJr <vdavis@siu.edu>
2015-11-05 04:41:29 +0000

    Good stuff

--- CS 220/Assignment#6/Palindrome.java
+++ CS 220/Assignment#6/Palindrome.java
@@ -13,18 +13,30 @@ import java.util.Scanner;
 public class Palindrome{
 
 	public static boolean palinCheck(String stuff){
-		if(stuff.length() == 0 || stuff.length() == 1)
+		if(stuff.length() == 0 || stuff.length() == 1){
 			return true;
-		if(stuff.charAt(0) == stuff.charAt(stuff.length() - 1))
+		}
+		else if(stuff.charAt(0) == stuff.charAt(stuff.length() - 1)){
 			return palinCheck(stuff.substring(1, stuff.length()-1));
-		return false;
+		}
+		else{
+			return false;
+		}
 	}
+	
 	//Number 5
 	public int count7(int n){
-		
+		if(n == 0){
+				return 0;
+		  	}
+    	if (n%10 == 7)  
+    		return 1 + count7(n/10);
+    	else
+    		return 0 + count7(n/10);
 	}
 
 	public static void main(String[] args) {
+		Palindrome pal = new Palindrome();
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter a String: ");
 		String input = scan.nextLine();
@@ -32,5 +44,12 @@ public class Palindrome{
 			System.out.println(input + " is a palindrome");
 		else
 			System.out.println(input + " is not a palindrome");
+
+		//Number 5
+		System.out.println("---------------------------------------------");
+		System.out.print("Enter a number: ");
+		int in = scan.nextInt();
+		System.out.println(pal.count7(in));
+
 	}
 }
--- CS 220/Assignment#6/README.md
+++ CS 220/Assignment#6/README.md
@@ -44,5 +44,13 @@ public class Palindrome{
 
 
 ##5
-
+public int count7(int n){
+		if(n == 0){
+				return 0;
+		  	}
+    	if (n%10 == 7)  
+    		return 1 + count7(n/10);
+    	else
+    		return 0 + count7(n/10);
+	}
 
